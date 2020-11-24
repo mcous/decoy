02104a0cbdfc2e829502997e3c2fd29f09f388c7
Mike Cousins <mike@cousins.io>
2020-11-24 17:23:27 -0500

    fixup: fix nested mocks

--- tests/common.py
+++ tests/common.py
@@ -12,6 +12,39 @@ class SomeClass:
         """Get the bar bool based on a few inputs."""
         ...
 
+    def do_the_thing(self, flag: bool) -> None:
+        """Perform a side-effect without a return value."""
+        ...
+
+
+class SomeNestedClass:
+    """Nested testing class."""
+
+    def foo(self, val: str) -> str:
+        """Get the foo string."""
+        ...
+
+    @property
+    def child(self) -> SomeClass:
+        """Get the child instance."""
+        ...
+
+
+class SomeAsyncClass:
+    """Async testing class."""
+
+    async def foo(self, val: str) -> str:
+        """Get the foo string."""
+        ...
+
+    async def bar(self, a: int, b: float, c: str) -> bool:
+        """Get the bar bool based on a few inputs."""
+        ...
+
+    async def do_the_thing(self, flag: bool) -> None:
+        """Perform a side-effect without a return value."""
+        ...
+
 
 def some_func(val: str) -> str:
     """Test function."""
--- tests/test_verify.py
+++ tests/test_verify.py
@@ -2,7 +2,7 @@
 import pytest
 
 from decoy import Decoy, matchers
-from .common import SomeClass, some_func
+from .common import SomeClass, SomeNestedClass, some_func
 
 
 def test_call_function_then_verify(decoy: Decoy) -> None:
@@ -51,3 +51,29 @@ def test_verify_with_matcher(decoy: Decoy) -> None:
 
     with pytest.raises(AssertionError):
         decoy.verify(stub(matchers.StringMatching("^ello")))
+
+
+def test_call_nested_method_then_verify(decoy: Decoy) -> None:
+    """It should be able to verify a past nested method call."""
+    stub = decoy.create_decoy(spec=SomeNestedClass)
+
+    stub.child.foo("hello")
+    stub.child.bar(0, 1.0, "2")
+
+    decoy.verify(stub.child.foo("hello"))
+    decoy.verify(stub.child.bar(0, 1.0, "2"))
+
+    with pytest.raises(AssertionError):
+        decoy.verify(stub.foo("fizzbuzz"))
+
+
+def test_call_no_return_method_then_verify(decoy: Decoy) -> None:
+    """It should be able to verify a past void method call."""
+    stub = decoy.create_decoy(spec=SomeClass)
+
+    stub.do_the_thing(True)
+
+    decoy.verify(stub.do_the_thing(True))  # type: ignore[func-returns-value]
+
+    with pytest.raises(AssertionError):
+        decoy.verify(stub.do_the_thing(False))  # type: ignore[func-returns-value]
