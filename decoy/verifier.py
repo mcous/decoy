f8abd338c0b5c213cb393517cbdf21ce53654756
Mike Cousins <mike@opentrons.com>
2021-08-10 14:23:18 -0400

    fixup: make sure times=0 still works

--- decoy/verifier.py
+++ decoy/verifier.py
@@ -14,7 +14,18 @@ class Verifier:
         calls: Sequence[SpyCall],
         times: Optional[int] = None,
     ) -> None:
-        """Verify that a list of calls satisfies a given list of rehearsals."""
+        """Verify that a list of calls satisfies a given list of rehearsals.
+
+        Arguments:
+            rehearsals: Rehearsal calls to verify.
+            calls: All calls made to the spies in the rehearsals list.
+            times: Number of times the rehearsal sequence should appear in calls.
+                If omitted, will look for "at least once."
+
+        Raises:
+            VerifyError: the actual calls to the spy did not match the given
+                rehearsals.
+        """
         match_count = 0
 
         for i in range(len(calls)):
@@ -23,7 +34,9 @@ class Verifier:
             if calls_subset == rehearsals:
                 match_count = match_count + 1
 
-        if match_count == 0 or (times is not None and match_count != times):
+        calls_verified = match_count != 0 if times is None else match_count == times
+
+        if not calls_verified:
             raise VerifyError(
                 rehearsals=rehearsals,
                 calls=calls,
--- tests/test_verifier.py
+++ tests/test_verifier.py
@@ -64,6 +64,16 @@ verify_raise_specs = [
         ],
         times=1,
     ),
+    VerifySpec(
+        rehearsals=[
+            VerifyRehearsal(spy_id=101, spy_name="spy_101", args=(1, 2, 3), kwargs={}),
+        ],
+        calls=[
+            SpyCall(spy_id=101, spy_name="spy_101", args=(1, 2, 3), kwargs={}),
+            SpyCall(spy_id=101, spy_name="spy_101", args=(1, 2, 3), kwargs={}),
+        ],
+        times=0,
+    ),
 ]
 
 verify_pass_specs = [
@@ -119,6 +129,13 @@ verify_pass_specs = [
             SpyCall(spy_id=101, spy_name="spy_101", args=(1, 2, 3), kwargs={}),
         ],
         times=1,
+    ),
+    VerifySpec(
+        rehearsals=[
+            VerifyRehearsal(spy_id=101, spy_name="spy_101", args=(1, 2, 3), kwargs={}),
+        ],
+        calls=[],
+        times=0,
     ),
 ]
 
