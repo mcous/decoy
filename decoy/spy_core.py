f9036bbbf0e086c770e4d125537988e53ef4c03d
Mike Cousins <mike@cousins.io>
2022-09-25 15:21:29 -0400

    fixup: remove print

--- decoy/spy_core.py
+++ decoy/spy_core.py
@@ -145,11 +145,7 @@ def _resolve_source(source: Any) -> Any:
     """Resolve the source object, unwrapping any generic aliases."""
     origin = inspect.getattr_static(source, "__origin__", None)
 
-    if origin is not None:
-        print("HEY", source, origin)
-        return origin
-
-    return source
+    return origin if origin is not None else source
 
 
 def _get_name(source: Any) -> str:
